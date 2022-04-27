a36433a7addfa1c84dfad950343d2876949b8e1e
Bongani Moyo <C20309081@mytudublin.ie>
2022-04-27 16:32:14 +0100

    class menatode created and getters, setters, constructor created

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -8,6 +8,55 @@ import processing.data.TableRow;
 
 public class NematodeVisualiser extends PApplet
 {
+	public class Nematodes
+	{
+		private String name;
+		private Integer length;
+		private boolean limbs;
+		private boolean gender;
+		private boolean eyes;
+		
+		//constructor for the nematodes
+		public Nematodes(String name, Integer length, boolean limbs, boolean gender, boolean eyes) {
+			this.name = name;
+			this.length = length;
+			this.limbs = limbs;
+			this.gender = gender;
+			this.eyes = eyes;
+		}
+
+		//getters and setters for the mematodes
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public Integer getLength() {
+			return length;
+		}
+		public void setLength(Integer length) {
+			this.length = length;
+		}
+		public boolean isLimbs() {
+			return limbs;
+		}
+		public void setLimbs(boolean limbs) {
+			this.limbs = limbs;
+		}
+		public boolean isGender() {
+			return gender;
+		}
+		public void setGender(boolean gender) {
+			this.gender = gender;
+		}
+		public boolean isEyes() {
+			return eyes;
+		}
+		public void setEyes(boolean eyes) {
+			this.eyes = eyes;
+		}
+	}
 
 	public void keyPressed()
 	{		
@@ -15,7 +64,6 @@ public class NematodeVisualiser extends PApplet
 		{
 		}		
 	}
-
 
 	public void settings()
 	{
@@ -29,11 +77,9 @@ public class NematodeVisualiser extends PApplet
 		smooth();				
 	}
 	
-
 	public void loadNematodes()
 	{
 	}
-
 
 	public void draw()
 	{	
